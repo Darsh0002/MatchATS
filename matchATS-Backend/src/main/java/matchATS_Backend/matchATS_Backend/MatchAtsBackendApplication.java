54bf40e1a2382d133a0f95df218cbdb7d2d3aa16
Darsh_Balar <darshbalar2005@gmail.com>
2025-12-27 15:01:57 +0530

    Initial Spring Boot Configuration

---
+++ matchATS-Backend/src/main/java/matchATS_Backend/matchATS_Backend/MatchAtsBackendApplication.java
@@ -0,0 +1,13 @@
+package matchATS_Backend.matchATS_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchAtsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchAtsBackendApplication.class, args);
+	}
+
+}
---
+++ matchATS-Backend/src/test/java/matchATS_Backend/matchATS_Backend/MatchAtsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package matchATS_Backend.matchATS_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchAtsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
