c46813b2d5edd68df339e84f811170aadfb63a03
Darsh_Balar <darshbalar2005@gmail.com>
2025-12-28 14:36:09 +0530

    Fixed Backend Endpoints

--- matchATS-Backend/src/main/java/com/darsh/MatchATS_Backend/controller/ResumeController.java
+++ matchATS-Backend/src/main/java/com/darsh/MatchATS_Backend/controller/ResumeController.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/resumes")
+@RequestMapping("/resumes")
 @CrossOrigin("*")
 public class ResumeController {
 
